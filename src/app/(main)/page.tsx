d07209dacc7fbb8fbbeed5c502804eb1d9f16d6d
Brendan Cullen <brendancullen996@gmail.com>
2023-11-06 18:09:18 -0500

    fix spacing in call to actions on main page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -20,6 +20,7 @@ const Home = () => {
             <li><b>Consign</b> items you no longer need</li>
             <li><b>Browse</b> our selection of repurposed clothing and materials</li>
           </ul>
+          <br />
           <p>
             With Stitches, you can look good and feel good about doing your part for the planet.
           </p>
@@ -32,12 +33,15 @@ const Home = () => {
       <CallToAction
         headerText="Our Mission"
         body={
-          <p>
-            Stitches began in 2023 with a simple idea: <b>that repairing is better than buying.</b>
+          <>
+            <p>
+              Stitches began in 2023 with a simple idea: <b>that repairing is better than buying.</b>
+            </p>
             <br />
-            <br />
-            We're on a mission to reduce textile waste and promote sustainable fashion practices, one stitch at a time.
-          </p>
+            <p>
+              We're on a mission to reduce textile waste and promote sustainable fashion practices, one stitch at a time.
+            </p>
+          </>
         }
         actionText="Repair Services"
         actionHref="/repair"
@@ -48,12 +52,12 @@ const Home = () => {
         headerText="Store Hours"
         body={
           <ul>
-            <li>Mon | closed</li>
+            <li>Mon  | closed</li>
             <li>Tues | closed</li>
-            <li>Wed | 10am - 6pm</li>
+            <li>Wed  | 10am - 6pm</li>
             <li>Thur | 10am - 6pm</li>
-            <li>Fri |  10am - 6pm</li>
-            <li>Sat |  10am - 6pm</li>
+            <li>Fri  | 10am - 6pm</li>
+            <li>Sat  | 10am - 6pm</li>
           </ul>
         }
         actionText="Contact Us"
