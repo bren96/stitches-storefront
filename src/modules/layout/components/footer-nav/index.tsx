bfe56f66399743fa0592a0481b14305e5ffa8360
Brendan Cullen <brendancullen996@gmail.com>
2023-11-09 22:55:56 -0500

    update footer

--- src/modules/layout/components/footer-nav/index.tsx
+++ src/modules/layout/components/footer-nav/index.tsx
@@ -8,7 +8,15 @@ import BrandLogo from "../brand-logo"
 
 const FooterNav = () => {
   const { collections } = useCollections()
-  const { product_categories } = useProductCategories()
+
+  const mens_product_categories = useProductCategories({
+    handle: 'mens'
+  })
+
+
+  const womens_product_categories = useProductCategories({
+    handle: 'womens'
+  })
 
   return (
     <div className="content-container flex flex-col gap-y-8 pt-16 pb-8 bg-white">
@@ -17,11 +25,26 @@ const FooterNav = () => {
           <BrandLogo withWording={true} />
         </div>
         <div className="text-small-regular grid grid-cols-3 gap-x-10 md:gap-x-16">
-          {product_categories && (
+          {collections && (
             <div className="flex flex-col gap-y-2">
-              <span className="text-base-semi">Categories</span>
+              <span className="text-base-semi">Collections</span>
+              <ul
+                className={clsx("grid grid-cols-1 gap-2", {
+                  "grid-cols-2": (collections?.length || 0) > 3,
+                })}
+              >
+                {collections?.slice(0, 6).map((c) => (
+                  <li key={c.id}>
+                    <Link className={clsx("ml-3")} href={`/collections/${c.handle}`}>{c.title}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+          {womens_product_categories && (
+            <div className="flex flex-col gap-y-2">
               <ul className="grid grid-cols-1 gap-2">
-                {product_categories?.slice(0, 6).map((c) => {
+                {womens_product_categories.product_categories?.slice(0, 6).map((c) => {
                   if (c.parent_category) {
                     return
                   }
@@ -36,7 +59,7 @@ const FooterNav = () => {
                   return (
                     <li className="flex flex-col gap-2" key={c.id}>
                       <Link
-                        className={clsx(children && "text-small-semi")}
+                        className={clsx(children && "text-base-semi")}
                         href={`/${c.handle}`}
                       >
                         {c.name}
@@ -59,54 +82,48 @@ const FooterNav = () => {
               </ul>
             </div>
           )}
-          {collections && (
+          {mens_product_categories && (
             <div className="flex flex-col gap-y-2">
-              <span className="text-base-semi">Collections</span>
-              <ul
-                className={clsx("grid grid-cols-1 gap-2", {
-                  "grid-cols-2": (collections?.length || 0) > 3,
+              <ul className="grid grid-cols-1 gap-2">
+                {mens_product_categories.product_categories?.slice(0, 6).map((c) => {
+                  if (c.parent_category) {
+                    return
+                  }
+
+                  const children =
+                    c.category_children?.map((child) => ({
+                      name: child.name,
+                      handle: child.handle,
+                      id: child.id,
+                    })) || null
+
+                  return (
+                    <li className="flex flex-col gap-2" key={c.id}>
+                      <Link
+                        className={clsx(children && "text-base-semi")}
+                        href={`/${c.handle}`}
+                      >
+                        {c.name}
+                      </Link>
+                      {children && (
+                        <ul className="grid grid-cols-1 ml-3 gap-2">
+                          {children &&
+                            children.map((child) => (
+                              <li key={child.id}>
+                                <Link href={`/${child.handle}`}>
+                                  {child.name}
+                                </Link>
+                              </li>
+                            ))}
+                        </ul>
+                      )}
+                    </li>
+                  )
                 })}
-              >
-                {collections?.slice(0, 6).map((c) => (
-                  <li key={c.id}>
-                    <Link href={`/collections/${c.handle}`}>{c.title}</Link>
-                  </li>
-                ))}
               </ul>
             </div>
           )}
-          <div className="flex flex-col gap-y-2">
-            <span className="text-base-semi">Medusa</span>
-            <ul className="grid grid-cols-1 gap-y-2">
-              <li>
-                <a
-                  href="https://github.com/medusajs"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  GitHub
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://docs.medusajs.com"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Documentation
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://github.com/medusajs/nextjs-starter-medusa"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Source code
-                </a>
-              </li>
-            </ul>
-          </div>
+
         </div>
       </div>
       <div className="flex flex-col-reverse gap-y-4 justify-center xsmall:items-center xsmall:flex-row xsmall:items-end xsmall:justify-between">
