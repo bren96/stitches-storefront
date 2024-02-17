3d08e19f613c98cb8d1c7f7a82f334b1647f0d9b
Brendan Cullen <brendancullen996@gmail.com>
2024-02-17 17:40:04 -0500

    turn off cart and account in nav

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -30,13 +30,14 @@ const Nav = () => {
           </div>
 
           <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
-            <div className="hidden small:flex items-center gap-x-6 h-full">
+            {/* <div className="hidden small:flex items-center gap-x-6 h-full">
               {process.env.FEATURE_SEARCH_ENABLED && <DesktopSearchModal />}
               <Link href="/account">
                 <User />
               </Link>
             </div>
-            <CartDropdown />
+            <CartDropdown /> */}
+            Comming Soon!
           </div>
         </nav>
         <MobileMenu />
